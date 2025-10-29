ba233be01a4de16558ae0f51996aeaaae5e58d39
sptryogi <115679084+sptryogi@users.noreply.github.com>
2025-10-29 11:45:21 +0700

    Update Ask.jsx

--- frontend/src/pages/Ask.jsx
+++ frontend/src/pages/Ask.jsx
@@ -114,25 +114,27 @@ export default function Ask() {
         </button>
       </div>
 
-      {answer && (
-        <div className="answer-box" style={{ marginTop: "20px" }}>
-          <h4>Jawaban:</h4>
-          {/* <p dangerouslySetInnerHTML={{ __html: answer.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>") }}></p> */}
-          const isArabic = /[\u0600-\u06FF]/.test(answer);
-          <p
-            className={isArabic ? "arabic-text" : ""}
-            dangerouslySetInnerHTML={{
-              __html: answer.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>")
-            }}
-          ></p>
-          {audioUrl && (
-            <audio controls style={{ marginTop: "10px" }}>
-              <source src={audioUrl} type="audio/mpeg" />
-              Browser kamu tidak mendukung audio.
-            </audio>
-          )}
-        </div>
-      )}
+      {answer && (() => {
+        const isArabic = /[\u0600-\u06FF]/.test(answer);
+        return (
+          <div className="answer-box" style={{ marginTop: "20px" }}>
+            <h4>Jawaban:</h4>
+            <p
+              className={isArabic ? "arabic-text" : ""}
+              dangerouslySetInnerHTML={{
+                __html: answer.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>")
+              }}
+            ></p>
+      
+            {audioUrl && (
+              <audio controls style={{ marginTop: "10px" }}>
+                <source src={audioUrl} type="audio/mpeg" />
+                Browser kamu tidak mendukung audio.
+              </audio>
+            )}
+          </div>
+        );
+      })()}
     </div>
   );
 }
