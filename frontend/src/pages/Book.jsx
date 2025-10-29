1b3fd8538385226f91ca0af97c40ca3124dda00d
sptryogi <115679084+sptryogi@users.noreply.github.com>
2025-10-29 14:58:11 +0700

    Update Book.jsx

--- frontend/src/pages/Book.jsx
+++ frontend/src/pages/Book.jsx
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Document, Page, pdfjs } from "react-pdf";
 
-// wajib untuk worker pdf.js
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
-pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 import 'react-pdf/dist/esm/Page/TextLayer.css';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
 
